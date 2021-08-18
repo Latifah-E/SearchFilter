7f123351a08f9585e8ba49bea47bb96cc8b4aa70
Daudy <latifahmtoro@gmail.com>
2021-08-18 11:25:58 +0300

    added v-bind

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var app = new Vue({
     data() {
         return{
        answers:[{
-        title:'How can i pay?', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris finibus venenatis justo in rutrum. Aenean ac augue nulla. Duis vel velit nec lorem blandit tempor a sed nulla. Donec nibh tellus, malesuada et libero posuere, efficitur elementum massa. Maecenas convallis dolor non scelerisque mollis. Phasellus vitae porttitor nulla. Ut porta neque urna, et interdum lacus vestibulum vitae. Sed tincidunt, mauris non aliquet condimentum, massa elit ultrices ex, nec vestibulum magna eros eget nisl. Donec sodales et lectus ac vestibulum. In fringilla, ex at mattis tincidunt, metus ipsum faucibus nisl, id sollicitudin urna purus vel est.'
+        title:'How can i pay?', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris finibus venenatis justo in rutrum. Aenean ac augue nulla. Duis vel velit nec lorem blandit tempor a sed nulla. Donec nibh tellus, malesuada et libero posuere, efficitur elementum massa. Maecenas convallis dolor non scelerisque mollis. Phasellus vitae porttitor nulla. Ut porta neque urna, et interdum lacus vestibulum vitae. Sed tincidunt, mauris non aliquet condimentum, massa elit ultrices ex, nec vestibulum magna eros eget nisl. Donec sodales et lectus ac vestibulum. In fringilla, ex at mattis tincidunt, metus ipsum faucibus nisl, id sollicitudin urna purus vel est latifa.'
        },
        {
         title:'How can i reqest for multiple fundis?', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris finibus venenatis justo in rutrum. Aenean ac augue nulla. Duis vel velit nec lorem blandit tempor a sed nulla. Donec nibh tellus, malesuada et libero posuere, efficitur elementum massa. Maecenas convallis dolor non scelerisque mollis. Phasellus vitae porttitor nulla. Ut porta neque urna, et interdum lacus vestibulum vitae. Sed tincidunt, mauris non aliquet condimentum, massa elit ultrices ex, nec vestibulum magna eros eget nisl. Donec sodales et lectus ac vestibulum. In fringilla, ex at mattis tincidunt, metus ipsum faucibus nisl, id sollicitudin urna purus vel est.'
@@ -18,24 +18,26 @@ var app = new Vue({
         title:'Is it possible to add a new category?', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris finibus venenatis justo in rutrum. Aenean ac augue nulla. Duis vel velit nec lorem blandit tempor a sed nulla. Donec nibh tellus, malesuada et libero posuere, efficitur elementum massa. Maecenas convallis dolor non scelerisque mollis. Phasellus vitae porttitor nulla. Ut porta neque urna, et interdum lacus vestibulum vitae. Sed tincidunt, mauris non aliquet condimentum, massa elit ultrices ex, nec vestibulum magna eros eget nisl. Donec sodales et lectus ac vestibulum. In fringilla, ex at mattis tincidunt, metus ipsum faucibus nisl, id sollicitudin urna purus vel est.'
        },
     ],
-    search: ''
+    search: '',
+    inPut: true
        
     }
     },
 computed: {
     filteredAnswers: function(){
-        var answerCounter = document.getElementById("input").value.length;
-        if (answerCounter <= 0){
-            return this.answers = null;
-            
-        }
-        else{
             return this.answers.filter((answer)=> {
                 return answer.title.match(this.search) || answer.content.match(this.search);
                 
             });
+
+            
+
             
         }
+},
+methods:{
+    isTrue(){
+        this.inPut = false
     }
 }
 });
